3545f6fdba900a49d1584940d803563d5902e358
Brian Chrisman <brchrisman@gmail.com>
2017-12-20 22:29:42 -0800

    integrate strsignal

--- src/signal-wrench.c
+++ src/signal-wrench.c
@@ -7,26 +7,73 @@
 extern char **environ;
 int main ( int argc, char **argv )
 {
-    // For testing purposes, we can set SIGPIPE-ignore here
-    char * foo = getenv("SCPIPEWRENCH_SIG");
-    if (foo != NULL && strcmp(foo, "SIG_IGN"))
+    char * signalHandlerDisposition = getenv("SIGNAL_WRENCH_DISPOSITION");
+    char * signalString = getenv("SIGNAL_WRENCH_SIGNAL_STRING");
+
+    if (signalHandlerDisposition)
     {
-        if (! unsetenv("SCPIPEWRENCH_SIG"))
+        if (! unsetenv("SIGNAL_WRENCH_DISPOSITION"))
         {
-            perror("Failed to clear environment variable SCPIPEWRENCH_SIG");
-            exit(1);
-        }
-        if (signal(SIGPIPE, SIG_IGN) == SIG_ERR)
-        {
-            perror("Failed to set SIGPIPE to ignore.");
+            perror("Failed to clear environment variable SIGNAL_WRENCH_DISPOSITION");
             exit(1);
         }
     }
     else
     {
-        if (signal(SIGPIPE, SIG_DFL) == SIG_ERR)
+        fprintf(stderr, "Must set SIGNAL_WRENCH_DISPOSITION.\n");
+        exit(1);
+    }
+
+    if (signalString)
+    {
+        if (! unsetenv("SIGNAL_WRENCH_SIGNAL_STRING"))
         {
-            perror("Failed to set SIGPIPE to default handler.");
+            perror("Failed to clear environment variable SIGNAL_WRENCH_SIGNAL_STRING");
+            exit(1);
+        }
+    }
+    else
+    {
+        fprintf(stderr, "Must set SIGNAL_SIGNAL_STRING.\n");
+        exit(1);
+    }
+
+    if (! strcmp(signalHandlerDisposition, "SIG_IGN") && ! strcmp(signalHandlerDisposition, "SIG_DFL"))
+    {
+        fprintf(stderr, "Disposition must be SIG_IGN or SIG_DFL, not (%s).\n", signalHandlerDisposition);
+        exit(1);
+    }
+
+    int i, signalNumber;
+
+    for (i = 1; i < 32; i++)
+    {
+        if (strcmp(signalString, strsignal(i))) // strncmp
+        {
+            signalNumber = i;
+            break;
+        }
+    }
+
+    if (i == 32)
+    {
+        fprintf(stderr, "No signal (%s) found.\n", signalString);
+        exit(1);
+    }
+
+    if (strcmp(signalHandlerDisposition, "SIG_IGN"))
+    {
+        if (signal(signalNumber, SIG_IGN) == SIG_ERR)
+        {
+            perror(strcat("Failed to set ignore handler for signal -- ", strsignal(signalNumber)));
+            exit(1);
+        }
+    }
+    else
+    {
+        if (signal(signalNumber, SIG_DFL) == SIG_ERR)
+        {
+            perror(strcat("Failed to set default handler for signal -- ", strsignal(signalNumber)));
             exit(1);
         }
     }
